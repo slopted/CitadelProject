64ce8068a2c33449df9fe2ecfeeb4b4dd1e048c2
Slopted <slopted@gmail.com>
2024-09-09 23:48:05 -0300

    gitignore file added2

--- variables.tf
+++ variables.tf
@@ -8,3 +8,27 @@ variable "resource_group_name" {
   default     = "rg-citadel-prod-002"
   description = "Name of the resource group."
 }
+
+variable "subscription_id" {
+  type        = string
+  default     = "61709a94-7ad9-4c00-a162-0fc36af79d80"
+  description = "Subcription_Id"
+}
+
+variable "tenant_id" {
+  type        = string
+  default     = "c5586ed5-1241-4338-a93d-272377f7b70c"
+  description = "tenant_id"
+}
+
+variable "client_id" {
+  type        = string
+  default     = "3304ede4-10f4-468b-b88c-6a33d59eb9ce"
+  description = "client_id"
+}
+
+variable "client_secret" {
+  type        = string
+  default     = "4248Q~DgKclhuvBXj8EZXIeQbylz3tiuGMFQhcW6"
+  description = "client_secret"
+}
