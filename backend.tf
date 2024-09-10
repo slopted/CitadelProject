b0e3661c5dc39c6fa80e53ccad0955923f3ac87c
Slopted <slopted@gmail.com>
2024-09-10 00:25:57 -0300

    tfstate commit to azure reorder

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-citadel-prod-001"
-    storage_account_name  = "stcitadelprod001"
-    container_name        = "terraform-state"
-    key                   = "terraform.tfstate"
-  }
-}
--- providers.tf
+++ providers.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-citadel-prod-001"
+    storage_account_name  = "stcitadelprod001"
+    container_name        = "terraform-state"
+    key                   = "terraform.tfstate"
+  }
+
+}
+
 provider "azurerm" {
   features {}
 }
